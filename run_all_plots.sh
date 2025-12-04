da297fbc412bcaab737f9a4d3ef1202708bf434a
AlessandroRuzza <alessandroruzza22@gmail.com>
2025-12-04 10:18:29 +0100

    Fix sh

--- run_all_plots.sh
+++ run_all_plots.sh
@@ -1,2 +1,2 @@
-echo "Use as M=X bash run_all_plots.sh"
-sbatch --job-name=M${M}_run_all_plots --output=M${M}_run_all_plots_%j.out --error=M${M}_run_all_plots_%j.err run_all_plots.sbatch
+echo "Use as M=X bash ./run_all_plots.sh"
+sbatch --job-name=M${M}_run_all_plots --output=M${M}_run_all_plots_%j.out --error=M${M}_run_all_plots_%j.err scripts/run_all_plots.sbatch
