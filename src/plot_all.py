ca9b77cfab3794597ab6d7ca0fa24c5e60893535
AlessandroRuzza <alessandroruzza22@gmail.com>
2025-12-10 03:52:17 +0100

    fix

--- src/plot_all.py
+++ src/plot_all.py
@@ -12,4 +12,8 @@ if __name__ == "__main__":
 
     comp_res_long = all_res_long
     comp_res_direct = direct.run_sims(comparison.make_direct_params_from_long(param_sets), args.skipThreshold)
+    for k, res in comp_res_direct.items():
+        for key in res["attempts_total"].keys():
+            res["attempts_total"][key] = [v*2 for v in res["attempts_total"][key]] # Adjust for time unit being travel time of 1 short link of longRange (1/2 of long link)
+
     comparison.plot_comparison(comp_res_long, comp_res_direct)
